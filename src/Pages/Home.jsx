9c65a4658b400409e44a231a6827994c5fa3cf51
Zakariyah robiat <zakariyahrobiat2018@gmail.com>
2023-03-08 15:50:20 +0000

    Revert "Revert "Revert "build the responsive nave bar and hero section"""

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import "./Home.sass";
-import Header from "../Components/header";
-import Hero from "../Components/hero";
-import About from "../Components/about";
 const Home = () => {
-  return (
-    <div>
-      <Header />
-      <Hero />
-      <About />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
