f13b162c3241eaddc78bf294e4562d4b78c3a8fc
Zakariyah robiat <zakariyahrobiat2018@gmail.com>
2023-03-08 15:44:57 +0000

    Revert "Revert "build the responsive nave bar and hero section""

---
+++ src/assests/data.jsx
@@ -0,0 +1,27 @@
+export const nav = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "Services",
+    link: "/Services",
+  },
+  {
+    id: 3,
+    name: "Works",
+    link: "/Works",
+  },
+  {
+    id: 4,
+    name: "Training",
+    link: "/Training",
+  },
+  {
+    id: 5,
+    name: "Contact us",
+    link: "Contact",
+  },
+];
